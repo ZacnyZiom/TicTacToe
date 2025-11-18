c199bafdea658227e397da5407500d3ab263d90e
ZacnyZiom <hashziom3k@tutanota.com>
2025-11-18 18:51:50 +0100

    Next try with files

--- obj/Debug/net8.0/TicTacToe.AssemblyInfo.cs
+++ obj/Debug/net8.0/TicTacToe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28423be5ebc64287812668a6ec35b0f6ffe0a087")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
